b3c890843762c9d092bb9b6d3176fc1a14c4b6e7
Gabrielle Edwards Account <10971828@uvu.edu>
2022-10-23 16:07:48 -0600

    about to start images

---
+++ unit05/script.js
@@ -0,0 +1,8 @@
+console.log("we are connected");
+function toggleMenu() {
+  document.getElementById("primaryNav").classList.toggle("open");
+}
+
+const x = document.getElementById("hamburgerButton");
+
+x.onclick = toggleMenu;
