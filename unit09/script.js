535c2357a61a12585bca56bd0ebf1ae15e21cae4
Gabrielle Edwards Account <10971828@uvu.edu>
2022-11-22 11:29:38 -0700

    doing final checks

--- unit09/script.js
+++ unit09/script.js
@@ -1,15 +1,23 @@
-const swiper = new Swiper(".swiper", {
-  // Optional parameters
-  direction: "horizontal",
-  speed: 2000,
-  loop: true,
+// const swiper = new Swiper(".swiper", {
+//   // Optional parameters
+//   direction: "horizontal",
+//   speed: 2000,
+//   loop: true,
 
-  // If we need pagination
-  pagination: {
-    el: ".swiper-pagination",
-  },
-  autoplay: {
-    delay: 5000,
-  },
-});
-console.log(Swiper);
+//   // If we need pagination
+//   pagination: {
+//     el: ".swiper-pagination",
+//   },
+//   autoplay: {
+//     delay: 5000,
+//   },
+// });
+// console.log(Swiper);
+
+function toggleMenu() {
+  document.getElementById("primaryNav").classList.toggle("open");
+}
+
+const x = document.getElementById("hamburgerBtn");
+
+x.onclick = toggleMenu;
