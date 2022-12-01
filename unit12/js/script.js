de059d31b162d26bfc6b97a584717f69b82649f7
Gabrielle Edwards Account <10971828@uvu.edu>
2022-11-30 17:07:27 -0700

    stuck :(

--- unit12/js/script.js
+++ unit12/js/script.js
@@ -1,9 +1,19 @@
-// import
-
 const apiURL = "./hoteldata.json";
 
 fetch(apiURL)
   .then((response) => response.json())
   .then((myList) => {
     console.log(myList);
+
+    let myImageTag = document.createElement("img");
+    myImageTag.src = myList[0].photo;
+
+    let myCaptionTag = document.createElement("figcaption");
+    myCaptionTag.textContent = myList[0].name;
+
+    let myFigureTag = document.createElement("figure");
+    myFigureTag.appendChild(myImageTag);
+    myFigureTag.appendChild(myCaptionTag);
+
+    document.getElementById("myCards").appendChild(myImageTag);
   });
